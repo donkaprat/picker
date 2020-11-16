90cf71cf4b62f1ba7ce75cde9601c5594a46bc8b
f.zemma <f.zemma@kreactive.com>
2020-11-16 20:56:42 +0100

    donka

--- picklist.js
+++ picklist.js
@@ -1,4 +1,4 @@
-const productList = [
+let productList = [
     { img: "fh0g3", name: "Bamboo Watch", category: "Accessories", stock: "Instock" },
     { img: "el433", name: "Black Watch", category: "Accessories", stock: "Instock" },
     { img: "cz3c1", name: "Blue Band", category: "Fitness", stock: "Lowstok" },
@@ -6,38 +6,76 @@ const productList = [
     { img: "6wer53", name: "Bracelet", category: "Accessories", stock: "Lowstok" },
     { img: "31fwg", name: "Brown Purse", category: "Accessories", stock: "Instock" }
 ];
-let pushProductList = ['img', 'name', 'category', 'stock'];
 let Listpush = [];
 let app = document.getElementById('app');
 let htmlCode = `<h1>Pick list</h1>`;
+let valueSelectionned = null;
+let valueIdList1 = null;
+let valueIdList2 = null;
+const selectValueList1 = (e) => {
+    valueIdList1 = e.target.value;
+};
+const selectValueList2 = (e) => {
+    valueIdList2 = e.target.value;
+};
+const addEventOnOption = (list) => {
+    if (list == productList) {
+        list.forEach((element, i) => {
+            document.getElementById(`option-${i}`).addEventListener("click", selectValueList1);
+        });
+    }
+    else if (list == Listpush) {
+        list.forEach((element, i) => {
+            document.getElementById(`option-list2${i}`).addEventListener("click", selectValueList2);
+        });
+    }
+};
 const list = () => {
     let tr = "<div>" + productList.map((object, i) => {
-        return `<option> ${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
+        return `<option id = option-${i} value=${i}> ${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
     }).join("") + "</div>";
     document.getElementById('tableBody').innerHTML = tr;
+    addEventOnOption(productList);
+};
+const list2 = () => {
+    let tr = "<div>" + Listpush.map((object, i) => {
+        return `<option id = option-list2${i} value=${i}> ${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
+    }).join("") + "</div>";
+    document.getElementById('tableBody2').innerHTML = tr;
+    addEventOnOption(Listpush);
 };
 const addAll = () => {
-    productList.forEach((e) => {
-        Listpush.push(e);
-        const list2 = () => {
-            let tr = "<div>" + Listpush.map((object) => {
-                return `<option>${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
-            }).join("") + "</div>";
-            document.getElementById('tableBody2').innerHTML = tr;
-        };
-        list2();
-    });
+    if (Listpush.length === 0) {
+        Listpush = productList;
+        productList = [];
+    }
+    else if (productList.length === 0) {
+        productList = Listpush;
+        Listpush = [];
+    }
+    list();
+    list2();
 };
 function addone() {
-    // let number = document.querySelectorAll('[name=1]')
-    Listpush.push();
-    const list2 = () => {
-        let tr = "<div>" + productList.map((object, i) => {
-            return `<option>${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
-        }).join("") + "</div>";
-        document.getElementById('tableBody2').innerHTML = tr;
-    };
+    if (valueIdList1 != null) {
+        valueSelectionned = productList[valueIdList1];
+    }
+    else {
+        valueSelectionned = Listpush[valueIdList2];
+    }
+    if (!Listpush.includes(valueSelectionned)) {
+        productList.splice(valueIdList1, 1);
+        Listpush.push(valueSelectionned);
+        valueIdList1 = null;
+    }
+    else {
+        valueSelectionned = Listpush[valueIdList2];
+        Listpush.splice(valueIdList2, 1);
+        productList.push(valueSelectionned);
+        valueIdList2 = null;
+    }
     list2();
+    list();
 }
 htmlCode += `<div class="tables"><select size=10 id="tableBody" ></select>  <select size=10 id="tableBody2" >${Listpush}</select></div>`;
 htmlCode += `<button onClick="addone()"> addOne</button>`;
--- picklist.ts
+++ picklist.ts
@@ -1,4 +1,11 @@
-const productList: { img: string, name: string, category: string, stock: string }[] = [
+interface IProduct {
+    img : string;
+    name : string;
+    category : string;
+    stock : string;
+}
+
+let productList: IProduct[] = [
     { img: "fh0g3", name: "Bamboo Watch", category: "Accessories", stock: "Instock" },
     { img: "el433", name: "Black Watch", category: "Accessories", stock: "Instock" },
     { img: "cz3c1", name: "Blue Band", category: "Fitness", stock: "Lowstok" },
@@ -7,49 +14,87 @@ const productList: { img: string, name: string, category: string, stock: string 
     { img: "31fwg", name: "Brown Purse", category: "Accessories", stock: "Instock" }
 ]
 
-let pushProductList = ['img', 'name', 'category', 'stock'];
-let Listpush = [];
-
-let app = document.getElementById('app')
+let Listpush : IProduct[] = [];
+let app : HTMLElement = document.getElementById('app');
 let htmlCode: string = `<h1>Pick list</h1>`;
 
+let valueSelectionned : IProduct = null;
+let valueIdList1: number | null = null;
+let valueIdList2: number | null = null
 
-const list = () => {
+const selectValueList1 = (e) => {
+    valueIdList1 = e.target.value
+}
+
+const selectValueList2 = (e) => {
+    valueIdList2 = e.target.value
+}
+
+const addEventOnOption = (list : IProduct[]) : void => {
+    if(list == productList){
+        list.forEach(( element ,i)=> {
+            document.getElementById(`option-${i}`).addEventListener("click",selectValueList1)
+        })
+    }else if(list == Listpush){
+        list.forEach(( element ,i)=> {
+            document.getElementById(`option-list2${i}`).addEventListener("click",selectValueList2)
+        })
+    }
+}
+
+const list = () : void => {
     let tr = "<div>" + productList.map((object, i) => {
-        return `<option> ${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
+         return `<option id = option-${i} value=${i}> ${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
     }).join("") + "</div>";
     document.getElementById('tableBody').innerHTML = tr
+    addEventOnOption(productList)
+}
 
+const list2 = () : void => {
+    let tr = "<div>" + Listpush.map((object, i) => {
+         return `<option id = option-list2${i} value=${i}> ${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
+    }).join("") + "</div>";
+    document.getElementById('tableBody2').innerHTML = tr
+    addEventOnOption(Listpush)
 }
 
 
 
-const addAll = () => {
-    productList.forEach((e) => {
-        Listpush.push(e)
-        const list2 = () => {
-            let tr = "<div>" + Listpush.map((object) => {
-                return `<option>${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
-            }).join("") + "</div>";
-            document.getElementById('tableBody2').innerHTML = tr
-        }
-        list2()
-    })
-
-}
-
-function addone() {
-    // let number = document.querySelectorAll('[name=1]')
-    Listpush.push()
-    const list2 = () => {
-        let tr = "<div>" + productList.map((object, i) => {
-            return `<option>${object.img} •  ${object.category} • ${object.name} • ${object.stock}</option>`;
-        }).join("") + "</div>";
-        document.getElementById('tableBody2').innerHTML = tr
-    }
+const addAll = () : void => {
+ if(Listpush.length === 0){
+    Listpush = productList
+    productList = []
+ }else if(productList.length === 0){
+     productList = Listpush
+     Listpush = []
+ }
+    list()
     list2()
-
 }
+    
+
+    function addone() : void{
+        if(valueIdList1 != null){
+            valueSelectionned = productList[valueIdList1]
+        }else{
+            valueSelectionned = Listpush[valueIdList2]
+        }
+
+        if(!Listpush.includes(valueSelectionned)){
+            productList.splice(valueIdList1, 1)
+            Listpush.push(valueSelectionned);
+            valueIdList1 = null
+        }else{
+            valueSelectionned = Listpush[valueIdList2]
+            Listpush.splice(valueIdList2, 1)
+            productList.push(valueSelectionned);
+            valueIdList2 = null
+        }
+        list2();
+        list();
+     }
+
+
 
 
 htmlCode += `<div class="tables"><select size=10 id="tableBody" ></select>  <select size=10 id="tableBody2" >${Listpush}</select></div>`
